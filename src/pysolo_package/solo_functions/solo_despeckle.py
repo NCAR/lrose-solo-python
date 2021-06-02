8cb34e93b395814434ceea491425957661306b67
MarCarrot <60483997+MarCarrot@users.noreply.github.com>
2021-06-02 10:31:45 -0500

    made dgi_clip_gate optional parameter
    default value is length of input list

--- src/pysolo_package/solo_functions/solo_despeckle.py
+++ src/pysolo_package/solo_functions/solo_despeckle.py
@@ -6,37 +6,57 @@ from pysolo_package.utils.function_alias import aliases
 
 se_despeckle = aliases['despeckle']
 
-def despeckle(input_list, bad, a_speckle, dgi_clip_gate, boundary_mask_input, boundary_mask_all_true=False):
-	if (len(input_list) != len(boundary_mask_input)):
-		raise ValueError(("data size (%d) and mask size (%d) must be of equal size.") % (len(input_list), len(boundary_mask_input)))
+def despeckle(input_list, bad, a_speckle, boundary_mask_input, dgi_clip_gate=0, boundary_mask_all_true=False):
+    """ 
+        Performs a despeckle operation on a list of data (a single ray)
+        
+        Args:
+            input_list: A list containing float data.
+            bad_value: A float that represents a missing/invalid data point.
+            a_speckle: An integer that determines the number of contiguous good data considered a speckle
+            boundary_mask_input: A list of bools for masking valid/invalid values for input_list
+            (optional) dgi_clip_gate: An integer determines the end of the ray (default: length of input_list)
+            (optional) boundary_mask_all_true: setting this to True may yield more results in despeckle (default: False)
 
-	# set return type and arg types
-	se_despeckle.restype = None
-	se_despeckle.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.c_size_t, ctypes.c_float, ctypes.c_int, ctypes.c_size_t, ctypes.POINTER(ctypes.c_bool)]
+        Returns:
+            RadarData: object containing resultant 'data' and 'masks' lists.
 
-	boundary_mask_output = deepcopy(boundary_mask_input)
+        Throws:
+            ValueError: if input_list and boundary_mask_input are not equal in size
+    """
 
-	# retrieve size of input/output/mask array
-	data_length = len(input_list)
-	# create a ctypes type that is an array of floats of length from above
-	data_length_type = ctypes.c_float * data_length
-	# initialize an empty float array of length
-	output_array = ctypes_helper.initialize_float_array(data_length)
+    if (len(input_list) != len(boundary_mask_input)):
+        raise ValueError(("data size (%d) and mask size (%d) must be of equal size.") % (len(input_list), len(boundary_mask_input)))
 
-	# create a ctypes type that is an array of bools of length from above
-	boundary_length_type = ctypes.c_bool * data_length
+    # set return type and arg types
+    se_despeckle.restype = None
+    se_despeckle.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.c_size_t, ctypes.c_float, ctypes.c_int, ctypes.c_size_t, ctypes.POINTER(ctypes.c_bool)]
 
-	# if optional, last parameter set to True, then create a list of bools set to True of length from above
-	if boundary_mask_all_true:
-		boundary_mask_input = [True] * data_length
+    boundary_mask_output = deepcopy(boundary_mask_input)
 
-	# run C function, output_array is updated with despeckle results
-	se_despeckle(data_length_type(*input_list), output_array, ctypes.c_size_t(data_length), ctypes.c_float(bad), ctypes.c_int(a_speckle), ctypes.c_size_t(dgi_clip_gate), boundary_length_type(*boundary_mask_input))
+    # retrieve size of input/output/mask array
+    data_length = len(input_list)
+    # create a ctypes type that is an array of floats of length from above
+    data_length_type = ctypes.c_float * data_length
+    # initialize an empty float array of length
+    output_array = ctypes_helper.initialize_float_array(data_length)
 
-	# convert ctypes array to python list
-	output_list = ctypes_helper.array_to_list(output_array, data_length)
+    # create a ctypes type that is an array of bools of length from above
+    boundary_length_type = ctypes.c_bool * data_length
 
-	boundary_mask_output = ctypes_helper.update_boundary_mask(input_list, output_list, boundary_mask_output)
+    # if optional, last parameter set to True, then create a list of bools set to True of length from above
+    if boundary_mask_all_true:
+        boundary_mask_input = [True] * data_length
+    if dgi_clip_gate == 0:
+        dgi_clip_gate = data_length        
 
-	# returns the new data and masks packaged in an object
-	return radar_structure.RadarData(output_list, boundary_mask_output)
+    # run C function, output_array is updated with despeckle results
+    se_despeckle(data_length_type(*input_list), output_array, ctypes.c_size_t(data_length), ctypes.c_float(bad), ctypes.c_int(a_speckle), ctypes.c_size_t(dgi_clip_gate), boundary_length_type(*boundary_mask_input))
+
+    # convert ctypes array to python list
+    output_list = ctypes_helper.array_to_list(output_array, data_length)
+
+    boundary_mask_output = ctypes_helper.update_boundary_mask(input_list, output_list, boundary_mask_output)
+
+    # returns the new data and masks packaged in an object
+    return radar_structure.RadarData(output_list, boundary_mask_output)
