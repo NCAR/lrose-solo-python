12d7cd483a2af2f95133c4b1198134275ed9a334
Ammar S <60483997+asiddiqi18@users.noreply.github.com>
2021-07-28 13:05:13 -0500

    condensed logic

--- src/pysolo/c_wrapper/masked_op.py
+++ src/pysolo/c_wrapper/masked_op.py
@@ -28,12 +28,6 @@ def masked_func(func, masked_array, *args, boundary_mask=None, second_masked_arr
         mask = masked_array.mask.tolist()
         data_list = masked_array.tolist(missing)
 
-        # if using a second masked array, obtain the lists for it as well
-        second_data_list = None
-        if (second_masked_array is not None):
-            second_missing = second_masked_array.fill_value
-            second_data_list = second_masked_array.tolist(second_missing)
-
     except AttributeError as e:
         print("Expected a numpy masked array.")
         print(e)
@@ -42,28 +36,21 @@ def masked_func(func, masked_array, *args, boundary_mask=None, second_masked_arr
     output_data = []
     output_mask = []
 
-    # if function uses bad flags, add a new parameter for the flag array. Initially set to None.
-    argsLength = len(args)
-    if (usesBadFlags):
-        # in pysolo, the last required arg is for the bad flags.
-        argsList = list(args)
-        argsList.append(None)
-        args = tuple(argsList)
-
     # iterate through each ray
     for i in range(len(data_list)):
         input_data = data_list[i] # gates
         input_mask = mask[i] # mask for gates
-        if second_data_list != None: # if using second masked array, obtain gates for that
-            second_input_data = second_data_list[i]
+
+        if second_masked_array is not None: # if using second masked array, obtain gates for that
+            second_missing = second_masked_array.fill_value
+            second_input_data = second_masked_array.tolist(second_missing)[i]
             # in pysolo, second parameter of functions always designates data list for secondary masked list, for functions that have secondary masked arrays.
             func_ma = func(input_data, second_input_data, missing, *args, boundary_mask=boundary_mask)
+        elif usesBadFlags:
+            func_ma = func(input_data, missing, *args, input_mask, boundary_mask=boundary_mask)
         else:
-            if (usesBadFlags):
-                argsList = list(args)
-                argsList[argsLength] = input_mask # fill the input_mask for bad flags args.
-                args = tuple(argsList)
             func_ma = func(input_data, missing, *args, boundary_mask=boundary_mask)
+
         output_data.append(np.ma.getdata(func_ma)) # add data to output list of lists
         output_mask.append(np.ma.getdata(func_ma.mask)) # add mask as well
 
