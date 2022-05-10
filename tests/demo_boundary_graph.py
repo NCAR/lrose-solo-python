383c3d7fd320cc90783a3e497f6d9f45c832ef5a
Ammar <marmar18@live.com>
2022-05-10 16:52:16 -0500

    rhi

--- tests/demo_boundary_graph.py
+++ tests/demo_boundary_graph.py
@@ -1,14 +1,15 @@
-from pathlib import Path
 from datetime import datetime
-import pyart
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+import pyart
 import src.pysolo as solo
 
 # DATA_FILE_DIR = "tests/data/subset_data/sweep1_VEL_F_max_range_30/cfrad.20150626_000730.199_to_20150626_000800.311_SPOL_SUR.nc"
-DATA_FILE_DIR = "tests/data/radar_data_b.nc"
+DATA_FILE_DIR = "tests/data/cfrad.20150528_234131.942_to_20150528_234436.150_SPOL_v1993_PunRhi_RHI.nc"
 # BOUNDARY_FILE_DIR = "tests/data/subset_data/sweep1_VEL_F_max_range_30/fieldVEL_F-sweep1-Boundary1"
 BOUNDARY_FILE_DIR = "tests/data/Custom-boundary2"
-FIELD = "reflectivity"
+FIELD = "DBZ_F"
 
 
 def main():
@@ -18,15 +19,14 @@ def main():
     radar = pyart.io.read(path_to_data_file)
     print(radar.scan_type)
 
-    boundary_list_of_lists = solo.get_boundary_mask_from_file(radar, path_to_boundary_file)
-    # boundary_list_of_lists = solo.get_boundary_mask_from_list(radar, [-96, -80, -16, -96], [90, 13, 84, 90])
-    # boundary_list_of_lists = solo.get_boundary_mask_from_list(radar, [-50, 50, 50, -50],   [-50, -50, 50, 50])
+    # boundary_list_of_lists  = solo.get_boundary_mask_from_file(radar, path_to_boundary_file)
 
+    boundary_list_of_lists = solo.get_boundary_mask_from_list(radar, [-30, -20, -20, 30], [40, 40, 60, 60])
 
     display = pyart.graph.RadarMapDisplay(radar)
 
     from_km = 0
-    to_km = 400
+    to_km = 200
     kilometers_between_gates = radar.range['meters_between_gates'] / 1000
     ring_zapped_mask = solo.ring_zap_masked(radar.fields[FIELD]['data'], from_km, to_km, kilometers_between_gates, boundary_masks=boundary_list_of_lists)
     radar.add_field_like(FIELD, f'{FIELD}_ring_zapped', ring_zapped_mask, replace_existing=True)
@@ -38,7 +38,8 @@ def main():
     # display.set_limits(ylim=[-120, 0], xlim=[-100, 100], ax=ax[1])
     dt_string = datetime.now().strftime(r"%D %I:%M %p")
     plt.suptitle(f'{FIELD} ({dt_string})', fontsize=16)
-    plt.savefig('quick-test.png')
+    plt.show()
+    plt.savefig('outputs/boundary/quick-test.png')
 
 
 if __name__ == "__main__":
